2fa484b07b4d93dabeb764aba8474f85a48489f0
Sebastian Słoma <sebastiansloma@gmail.com>
2024-01-12 17:42:35 +0100

    added trees

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,50 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
 const Hero = () => {
-  return (
-    <div>
-        <Image src='/zombie_sign.png' alt='sign' width={400} height={300} />
-    </div>
-  )
-}
+	return (
+		<div>
+			<div className='h-screen'>
+                <div>
+                <Image
+					src='/tree3.png'
+					alt='sign'
+					width={400}
+					height={300}
+					className='relative z-[-1]'
+				/>
+                </div>
+				<Image
+					src='/tree2.png'
+					alt='sign'
+					width={400}
+					height={300}
+					className='relative z-[-1]'
+				/>
+				<Image
+					src='/zombie_sign.png'
+					alt='sign'
+					width={400}
+					height={300}
+					className='relative z-[-1]'
+				/>
+                <Image src='/tree1.png'
+					alt='tree1'
+					width={400}
+					height={300}
+					className='relative top-0 right-52 z-[1]'/>
+			</div>
+            <div>
+            <Image
+					src='/tree4.png'
+					alt='sign'
+					width={800}
+					height={400}
+					className='relative z-[-1]'
+				/>
+            </div>
+		</div>
+	);
+};
 
-export default Hero
+export default Hero;
