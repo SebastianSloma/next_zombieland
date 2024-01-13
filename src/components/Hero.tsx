b13325c82e161f06576cf0be09571df189c0c19d
Sebastian Słoma <sebastiansloma@gmail.com>
2024-01-13 17:27:53 +0100

    pos trees

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,42 +3,42 @@ import React from 'react';
 
 const Hero = () => {
 	return (
-		<div>
+		<div className='h-lvh'>
 			<Image
 				src='/tree3.png'
 				alt='sign'
-				width={1000}
+				width={800}
 				height={600}
-				className='absolute z-[-40] inset-x-0 top-0 '
+				className='absolute z-[40] top-0 left-1/2 transform -translate-x-1/2 '
 			/>
 			<Image
 				src='/tree2.png'
 				alt='sign'
-				width={400}
+				width={700}
 				height={300}
-				className='absolute z-[-30] inset-y-0 left-0'
+				className='absolute z-[30] top-1/2 left-0 transform -translate-y-1/2'
 			/>
 			<Image
 				src='/zombie_sign.png'
 				alt='sign'
 				width={400}
 				height={300}
-				className='absolute z-[-100] top-250 right-250 '
+				className='fixed z-[-100] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 '
 			/>
 			<Image
 				src='/tree1.png'
 				alt='tree1'
-				width={600}
-				height={300}
-				className='absolute inset-y-0 right-0   z-[10]'
+				width={570}
+				height={280}
+				className='absolute z-[10]  right-0 '
 			/>
 
 			<Image
 				src='/tree4.png'
 				alt='sign'
-				width={800}
+				width={500}
 				height={400}
-				className='absolute z-[-22] inset-x-0 bottom-0'
+				className='absolute z-[50] bottom-0 left-1/2 transform -translate-x-1/2'
 			/>
 		</div>
 	);
