15d463bea4f7173dc513322353b80448b9ac6f11
Sebastian Słoma <sebastiansloma@gmail.com>
2024-01-14 12:08:58 +0100

    added parallax effect to hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,94 +1,68 @@
 'use client';
 
-import React from 'react';
+import React, { useRef } from 'react';
 import { motion, useScroll, useTransform } from 'framer-motion';
 
 const Hero = () => {
-	const secRef = React.useRef<HTMLDivElement>(null);
-
+	const ref = useRef(null);
 	const { scrollYProgress } = useScroll({
-		target: secRef,
-		offset: ['start end', 'end start'],
+		target: ref,
+		offset: ['start start', 'end start'],
 	});
 
-	const scale = useTransform(scrollYProgress, [0, 0.5], [0, 1]);
-	const xTransform = useTransform(
-		scrollYProgress,
-		[1, 0.5, 0.1, 0],
-		[-1000, 0, 0, 0]
-	);
-	const scale2 = useTransform(scrollYProgress, [0, 0.5], [0, 2]);
-	const xTransform2 = useTransform(
-		scrollYProgress,
-		[1, 0.5, 0.1,0],
-		[1000, 0, 0, 0]
-	);
+	const { scrollY } = useScroll()
+	const x = useTransform(scrollY, [0, 500], [0, 1000])
+
+	const treeUp = useTransform(scrollYProgress, [0, 1], ['0%', '100%']);
+	const treeLeft = useTransform(scrollYProgress, [0, 1], ['0%', '200%']);
+	const sign = useTransform(scrollYProgress, [0, 1], ['0%', '50%']);
+	const treeRight = useTransform(scrollYProgress, [0, 1], ['0%', '200%']);
+
+
+
 	return (
-		<div className='h-lvh'>
+		<div
+			ref={ref}
+			className='w-full h-screen overflow-hidden relative grid place-items-center'
+			>
+			{/* tree up */}
 			<motion.img
 				src='/tree3.png'
 				alt='sign'
 				width={800}
 				height={600}
-				// className='absolute z-[40] top-0 left-1/2 transform -translate-x-1/2 '
-				style={{
-					position: 'absolute',
-					top: 0,
-					left: 0,
-					width: '100%',
-					scale: scale2,
-					x: xTransform2,
-				}}
+				className='relative z-40 '
+				style={{ y: treeUp }}
+				// className='relative z-10'
 			/>
+			{/* tree left */}
 			<motion.img
 				src='/tree2.png'
-				alt='sign'
+				alt='tree'
 				width={700}
 				height={300}
-				// className='absolute z-[30] top-1/2 left-0 transform -translate-y-1/2'
-				style={{
-					position: 'absolute',
-					top: 0,
-					left: 0,
-					width: '100%',
-					scale: scale,
-					x: xTransform,
-				}}
+				// className='relative z-30 top-1/2 left-0 transform -translate-y-1/2'
+				style={{ y: treeLeft}}
+				className='relative z-12  top-[-400px] left-[-400px]'
 			/>
+			{/* sign */}
 			<motion.img
 				src='/zombie_sign.png'
 				alt='sign'
 				width={400}
 				height={300}
-				// className='absolute z-[-100] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 '
-				style={{ position: "absolute", top: 0, left: 0, width: "100%", 	scale: scale,
-				x: xTransform, }}
+				className='absolute z-[-100] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 '
 			/>
+			{/* tree right */}
 			<motion.img
 				src='/tree1.png'
 				alt='tree1'
 				width={570}
 				height={280}
-				// className='absolute z-[10]  right-0 '
-				style={{
-					position: 'absolute',
-					top: 0,
-					left: 0,
-					width: '100%',
-					scale: scale,
-					x: xTransform,
-				}}
+				className='absolute z-[10]  right-0 '
+				style={{ x }}
 			/>
-
-			<motion.img
-				src='/tree4.png'
-				alt='sign'
-				width={500}
-				height={400}
-				// className='absolute z-[50] bottom-0 left-1/2 transform -translate-x-1/2'
-				style={{ position: "absolute", top: 0, left: 0, width: "100%", 	scale: scale,
-				x: xTransform, }}
-			/>
+			
 		</div>
 	);
 };
