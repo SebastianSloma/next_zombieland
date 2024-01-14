ad99c1402f90bdf4741f6194a0fa3131ebfc3db2
Sebastian Słoma <sebastiansloma@gmail.com>
2024-01-14 20:53:46 +0100

    added parallax to zombie one

--- src/components/ZombieOne.tsx
+++ src/components/ZombieOne.tsx
@@ -11,33 +11,57 @@ const ZombieOne = () => {
 	});
 
 	const scale = useTransform(scrollYProgress, [0, 0.5], [0.8, 1.1]);
-	const scaleTwo = useTransform(scrollYProgress, [0, 0.5], [0.8, 1]);
+	const scaleTwo = useTransform(scrollYProgress, [0, 0.5], [0.4, 1]);
+
+	const xTransform = useTransform(
+		scrollYProgress,
+		[1, 0, 0, 0],
+		[-1000, 0, 0, 0]
+	);
+	const xTransformTwo = useTransform(
+		scrollYProgress,
+		[1, 0.2, 0, 0],
+		[1000, 0, 0, 0]
+	);
 
 	return (
 		<div
 			className='w-full h-screen overflow-hidden relative grid place-items-center bg-gradient-to-r from-gray-300 to-gray-500'
 			ref={ref}>
+			{/* zombie right */}
 			<motion.img
 				src='/zombie1.png'
 				alt='zombie'
 				width={250}
 				height={200}
-				className='absolute z-20  top-[300px] right-[300px]'
+				className='absolute z-20  top-[300px] right-[-100px]'
+				style={{
+					x: xTransform,
+				}}
 			/>
+			{/* zombie left */}
 			<motion.img
 				src='/zombie2.png'
 				alt='zombie'
 				width={250}
 				height={200}
-				className='absolute z-30  top-[250px] left-[300px]'
+				className='absolute z-30  top-[240px] left-[-100px]'
+        style={{
+					x: xTransformTwo
+				}}
 			/>
+			{/* zombie middle */}
 			<motion.img
 				src='/zombie3.png'
 				alt='zombie'
-				width={200}
+				width={300}
 				height={200}
-				className='absolute z-20  top-[350px] left-[650px]'
+				className='absolute z-20  top-[200px] left-[650px]'
+				style={{
+					scale: scaleTwo,
+				}}
 			/>
+			{/* background */}
 			<motion.img
 				src='/forest.png'
 				alt='forest'
