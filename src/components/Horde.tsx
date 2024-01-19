9c7c5eaa64638811b0b919c643273384a14501ea
Sebastian Słoma <sebastiansloma@gmail.com>
2024-01-19 10:47:50 +0100

    pos and size imgs

--- src/components/Horde.tsx
+++ src/components/Horde.tsx
@@ -1,17 +1,48 @@
-import React from 'react'
-import Image from 'next/image'
+'use client';
+
+import React, { useRef } from 'react';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 const Horde = () => {
-  return (
-    <div>
+	return (
+		<div
+			className='w-full h-screen overflow-hidden relative grid place-items-center bg-gradient-to-r from-neutral-300 to-stone-600'
+			// ref={ref}
+      >
+			<motion.img
+				src='/cloud1.png'
+				alt='cloud'
+				width={400}
+				height={200}
+				className='absolute z-40 top-[0] left-[450px]'
+				style={{}}
+			/>
+			<motion.img
+				src='/cloud2.png'
+				alt='cloud'
+				width={400}
+				height={200}
+				className='absolute z-30 top-[0] right-[500px]'
+				style={{}}
+			/>
+			<motion.img
+				src='/moon.png'
+				alt='moon'
+				width={300}
+				height={200}
+				className='absolute z-[10] top-[0]'
+				style={{}}
+			/>
+			<motion.img
+				src='/horde2.png'
+				alt='zombie'
+				width={800}
+				height={200}
+				className='absolute z-5 bottom-[0]'
+				style={{}}
+			/>
+		</div>
+	);
+};
 
-        <Image src='/cloud1.png' alt='cloud' width={200} height={200}/>
-        <Image src='/cloud2.png' alt='cloud' width={200} height={200}/>
-        <Image src='/moon.png' alt='moon' width={200} height={200}/>
-        <Image src='/horde2.png' alt='zombie' width={200} height={200}/>
-   
-    </div>
-  )
-}
-
-export default Horde
+export default Horde;
