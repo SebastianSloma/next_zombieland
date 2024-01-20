63aee37112fbc41074b6d631420ea0ff8950df82
Sebastian Słoma <sebastiansloma@gmail.com>
2024-01-20 19:14:50 +0100

    added hand.tsx

--- src/components/Hand.tsx
+++ src/components/Hand.tsx
@@ -5,9 +5,9 @@ import Image from 'next/image'
 
 const Hand = () => {
   return (
-    <div>
-        <Image src='/sign.png' alt='sign' width={400} height={200}/>
-        <Image src='/and.png' alt='hand' width={400} height={200}/>
+    <div className='w-full h-screen overflow-hidden relative grid place-items-center bg-gradient-to-r from-lime-800 to-stone-500'>
+        <Image src='/sign.png' alt='sign' width={400} height={200} className='absolute z-20 top-[0] left-[400px]'/>
+        <Image src='/hand.png' alt='hand' width={600} height={500} className='absolute z-30 bottom-[0]'/>
     </div>
   )
 }
